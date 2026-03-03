360c1418cf2c7562ea7fe0e1567f51cbf61f1797
AndyGE44 <getiancheng115@gmail.com>
2026-03-03 17:42:06 -0500

    config.js Ok to be public

---
+++ front_end/config.js
@@ -0,0 +1,9 @@
+// config.js
+const CONFIG = {
+    SUPABASE_URL: 'https://jyzqnkiucgiufxfxjlqd.supabase.co',
+    SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp5enFua2l1Y2dpdWZ4ZnhqbHFkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEzNDYyMjIsImV4cCI6MjA4NjkyMjIyMn0.Sah476NknjgRFQ7D30O1YdhgwZpvACfbwt-B-4FNr-U',
+
+    VENDOR_ID: 3,                // 数据供应商 ID
+    ASSET_CLASS: 'stock',        // 资产类型筛选 (如果不需要筛选可以留空 '')
+    DEFAULT_TICKER: 'NVDA'       // 打开网页时默认展示的股票代码
+};
